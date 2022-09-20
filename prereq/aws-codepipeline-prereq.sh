df158220f1dc90959532eb52b766ea0da95861a3
edisonbhx <113216948+edisonbhx@users.noreply.github.com>
2022-09-20 13:22:18 -0500

    Update aws-codepipeline-prereq.sh

--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-east-1 \
---stack-name codepipeline-lambda-prereq \
+--stack-name codepipeline-lambda-prereq1 \
 --template-file ./aws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM
